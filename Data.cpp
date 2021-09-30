150c3cf7a3ee19b163f1858fd23ee400960072d8
Jacotucci <jacopo.tucci@stud.unifi.it>
2021-09-30 19:06:41 +0200

    Aggiunta metodi classe Data: dataToString()

--- Data.cpp
+++ Data.cpp
@@ -11,3 +11,26 @@ void Data::setDataAttuale() {
     setMesi(now->tm_mon);
     setAnni(now->tm_year + 1900);
 }
+
+std::string Data::dataToString() const{
+    std::string controlloData, dataString;
+        switch (visualizzazione) {
+            case 1:
+                // giorni settimana, mesi scritti
+                break;
+            case 2:
+                dataString += " giorno: " + std::to_string(giorni);
+                dataString += " mese: " + std::to_string(mesi);
+                dataString += " anno: " + std::to_string(anni);
+                break;
+            default:
+                controlloData = std::to_string(giorni);
+                dataString += (controlloData.length() == 1) ? ('0'+controlloData) : controlloData;
+                controlloData = " / " + std::to_string(mesi);
+                dataString += (controlloData.length() == 1) ? ('0'+controlloData) : controlloData;
+                dataString += " / " + std::to_string(anni);
+                break;
+        }
+        return dataString;
+}
+
--- Data.h
+++ Data.h
@@ -10,7 +10,7 @@
 
 class Data {
 public:
-    Data() = default;
+    explicit Data(int g = 0, int m = 0, int a = 0, int v = 0) : giorni(g), mesi(m), anni(a), visualizzazione(v) {}
 
     ~Data() = default;
 
@@ -18,32 +18,32 @@ public:
         return giorni;
     }
 
-    void setGiorni(int giorni) {
-        Data::giorni = giorni;
+    void setGiorni(int g) {
+        Data::giorni = g;
     }
 
     int getMesi() const {
         return mesi;
     }
 
-    void setMesi(int mesi) {
-        Data::mesi = mesi;
+    void setMesi(int m) {
+        Data::mesi = m;
     }
 
     int getAnni() const {
         return anni;
     }
 
-    void setAnni(int anni) {
-        Data::anni = anni;
+    void setAnni(int a) {
+        Data::anni = a;
     }
 
     void setDataAttuale(); //imposta la data di oggi
 
-    std::string dataToString(); //trasforma la data in una stringa da passare al display
+    std::string dataToString() const; //trasforma la data in una stringa da passare al display
 
 private:
-    int giorni, mesi, anni;
+    int giorni, mesi, anni, visualizzazione;
 
 };
 
